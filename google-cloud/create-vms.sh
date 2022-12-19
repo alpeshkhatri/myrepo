d3fd444039d05870ea3231256d5927340aed0c94
alpeshkhatri <alpeshkhatri>
2022-12-19 19:25:37 +0000

    gcloud setup

--- google-cloud/create-vms.sh
+++ google-cloud/create-vms.sh
@@ -1,7 +1,5 @@
 #/bin/bash
-ZONE=
-PROJECT=
-REGION=
+set -x 
 # gcloud compute regions list
 gcloud config set compute/region ${REGION}
 # gcloud computer zones list
@@ -10,7 +8,9 @@ gcloud config set compute/zone ${ZONE}
 for s in m1 w{1..3}
 do
 gcloud compute instances create $s
+gcloud compute ssh $s --command 'uname -a'
+gcloud compute scp install-docker.sh ${s}:/tmp
+gcloud compute ssh $s --command 'bash -x /tmp/install-docker.sh'
 done
 gcloud compute instances list
-gcloud compute ssh $s uname -a
 
