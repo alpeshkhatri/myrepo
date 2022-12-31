614cd368a8976faaac336536176e35a6816071c6
Your Name <you@example.com>
2022-12-31 00:07:55 +0000

    added stuff to run kafka

--- google-cloud/kafka/KafkaConsumerExample.java
+++ google-cloud/kafka/KafkaConsumerExample.java
@@ -25,61 +25,67 @@ public class KafkaConsumerExample {
         CommandLineParser parser = new DefaultParser();
         Option option_bootstrap = Option.builder("bootstrap")
             .required(true)
-            .desc("The bootstrap option")
+            .desc("bootstrap")
             .longOpt("bootstrap")
 	    .hasArg(true)
             .build();
         options.addOption(option_bootstrap);
+
         Option option_topic = Option.builder("topic")
             .required(true)
-            .desc("The r option")
+            .desc("topic")
             .longOpt("topic")
 	    .hasArg(true)
             .build();
         options.addOption(option_topic);
 
+        Option option_partition = Option.builder("partition")
+            .required(true)
+            .desc("partition")
+            .longOpt("partition")
+	    .hasArg(true)
+            .build();
+        options.addOption(option_partition);
+
+        Option option_offset = Option.builder("offset")
+            .required(true)
+            .desc("offset")
+            .longOpt("offset")
+	    .hasArg(true)
+            .build();
+        options.addOption(option_offset);
+
         try
         {
             commandLine = parser.parse(options, args);
 
-            if (commandLine.hasOption("bootstrap"))
-            {
-                System.out.print("Option bootstrap is present.  The value is: ");
-                System.out.println(commandLine.getOptionValue("bootstrap"));
-            }
-
-            if (commandLine.hasOption("topic"))
-            {
-                System.out.print("Option topic is present.  The value is: ");
-                System.out.println(commandLine.getOptionValue("topic"));
-            }
-
-            {
-                String[] remainder = commandLine.getArgs();
-                System.out.print("Remaining arguments: ");
-                for (String argument : remainder)
-                {
-                    System.out.print(argument);
-                    System.out.print(" ");
-                }
-
-                System.out.println();
-            }
+            System.out.print("Option bootstrap is present.  The value is: " + commandLine.getOptionValue("bootstrap"));
+            System.out.print("Option topic     is present.  The value is: " + commandLine.getOptionValue("topic"));
+            System.out.print("Option partition is present.  The value is: " + commandLine.getOptionValue("partition"));
+            System.out.print("Option offset    is present.  The value is: " + commandLine.getOptionValue("offset"));
 
     Properties props = new Properties();
-    // props.put("bootstrap.servers", "broker:9092");
     props.put("bootstrap.servers", commandLine.getOptionValue("bootstrap"));
     props.put("key.deserializer", StringDeserializer.class.getName());
     props.put("value.deserializer", StringDeserializer.class.getName());
+    // assign cannot take group id
     props.put("group.id", "test-group");
+    //
+    /*
+     * String topic = "foo";
+     * TopicPartition partition0 = new TopicPartition(topic, 0);
+     * TopicPartition partition1 = new TopicPartition(topic, 1);
+     * consumer.assign(Arrays.asList(partition0, partition1));
+     */
 
     KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-    //consumer.subscribe(Collections.singletonList("quickstart"));
     consumer.subscribe(Collections.singletonList(commandLine.getOptionValue("topic")));
+    //System.out.println("Connected to Kakfa as KafkaConsumer");
 
     try {
       while (true) {
         ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+	//System.out.println("in consumer.pool loop");
         for (ConsumerRecord<String, String> record : records) {
           System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
         }
